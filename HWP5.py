028c68d41d36f1ead9500714663602f32ef755dd
Nick Goodwill <nick.goodwill@yandex.ru>
2023-06-05 11:41:30 +0300

    forth task

--- HWP5.py
+++ HWP5.py
@@ -6,12 +6,12 @@
 # Input: 5
 # Output: yes
 
-# def is_prime(number):
-#     for i in range(2, int(number ** 0.5) + 1):
-#         if number % i == 0:
-#             return False
-#     return True
-# print(is_prime(int(input('Введите число: '))))
+def is_prime(number):
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+print(is_prime(int(input('Введите число: '))))
 
 # Задача №37.
 # Дано натуральное число N и
@@ -24,14 +24,14 @@
 # Input: 2 -> 3 4
 # Output: 4 3
 
-# def rev(n):
-#     if n > 0:
-#         rev (n-1)
-#         print(k[-n], end=" ")
-#
-# n = int(input("Введите число: "))
-# k = list(map(int, input("Введите числа через пробел: ").split()))
-# rev(n)
+def rev(n):
+    if n > 0:
+        rev (n-1)
+        print(k[-n], end=" ")
+
+n = int(input("Введите число: "))
+k = list(map(int, input("Введите числа через пробел: ").split()))
+rev(n)
 
 
 # Задача 26: Напишите программу, которая на вход принимает
@@ -39,18 +39,27 @@
 # помощью рекурсии.
 
 
-# def rev1(a,b):
-#     if b == 1:
-#         return (a)
-#     if b != 1:
-#         return (a * rev1(a, b-1))
-#
-# a = int(input("Введите число: "))
-# b = int(input("Введите число: "))
-# print("результат возедения в степень равен: ", rev1(a,b))
+def rev1(a,b):
+    if b == 1:
+        return (a)
+    if b != 1:
+        return (a * rev1(a, b-1))
+
+a = int(input("Введите число: "))
+b = int(input("Введите число: "))
+print("результат возедения в степень равен: ", rev1(a,b))
 
 
 # Задача 28: Напишите рекурсивную функцию sum(a, b),
 # возвращающую сумму двух целых неотрицательных чисел. Из
 # всех арифметических операций допускаются только +1 и -1.
 # Также нельзя использовать циклы.
+
+def sumdig(a, b):
+    if b == 0:
+        return a
+    return sumdig(a + 1, b - 1)
+
+a = int(input("Введите число: "))
+b = int(input("Введите число: "))
+print("Сумма чисел: ", sumdig(a, b))
